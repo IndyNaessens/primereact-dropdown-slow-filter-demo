7878c1b4f26a5edf41736c0ef896748ae00c2894
Indy Naessens <36234@delijn.be>
2024-08-05 15:21:54 +0200

    PrimeReact slow dropdown filter demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
+import { PrimeReactProvider } from "primereact/api";
 import type { AppProps } from "next/app";
 
+// PrimeReact
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.min.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <PrimeReactProvider>
+    <Component {...pageProps} />
+  </PrimeReactProvider>
 }
